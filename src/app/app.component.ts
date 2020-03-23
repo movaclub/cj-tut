deb53da5d2df74dab0b25d8efc88c0bcf2be20b5
mova.club <seeusers@gmail.com>
2020-03-23 16:52:28 +0200

    content div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cj-tut';
+export class AppComponent implements OnInit {
+
+  showContents: boolean;
+
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.showContents = false;
+  }
+
+  contentShow(): void {
+    this.showContents = true;
+  }
+  contentHide(): void {
+    setTimeout( () => {this.showContents = false; }, 1567);
+  }
+
 }
